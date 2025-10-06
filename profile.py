7ee414c8a4b4a3e8f0a2546e02e0de9de94b155f
saimeghanakuchana <saimeghanakuchana13@gmail.com>
2025-10-06 14:29:02 -0400

    Update profile.py
    Making changes in profile.py to give the local core network code

--- profile.py
+++ profile.py
@@ -1,77 +1,153 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+import os
+
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
+import geni.rspec.emulab.pnext as PN
+import geni.rspec.emulab.spectrum as spectrum
+
 
 tourDescription = """
-### Bring up an end-to-end 5G network with OpenAirInterface5G
+### OAI CN and RAN setup
 
-This profile was created by 
-
-It deploys a single compute node with a disk image that includes docker,
-docker-compose, tshark, and docker images for all of the OAI 5G core network 
-functions. It also includes source code and a prebuilt version of the OAI RAN 
-(gNB, nrUE, RF simulator).
-
-To use this profile, you should have reserved a `d430` server at Emulab in 
-advance.
+This profile instantiates an experiment for testing 
+- Server-class compute node (d430) with a Docker-based OAI 5G Core Network
+- A d430 compute node to host the core network
 
 """
 
 tourInstructions = """
 
-Note: After you instantiate an experiment, you have to wait until the POWDER
-portal shows your experiment status in green as "Your experiment is ready"
-before proceeding.
+Startup scripts will still be running when your experiment becomes ready.  Watch the "Startup" column on the "List View" tab for your experiment and wait until all of the compute nodes show "Finished" before proceeding.
 
+After all startup scripts have finished...
+
+On `cn5g-docker-host`, open a terminal session via SSH, or using the shell option for that node in the portal.
+
+Start the 5G core network services.
+
+```
+cd /var/tmp/oai-cn5g
+sudo docker compose up -d
+```
+ 
+It will take several seconds for the services to start up. Since we started the services in detached mode, you can check the status of the services with:
+
+```
+sudo docker compose ps
+```
+
+In another session, start following the logs for the AMF. This way you can see when the UE attaches to the network.
+
+```
+cd /var/tmp/oai-cn5g
+sudo docker compose logs -f oai-amf
+```
+
+If you'd like to monitor traffic between the various network functions and the gNodeB, start tshark in yet another session:
+
+```
+sudo tshark -i oai-cn5g \
+  -f "not arp and not port 53 and not host archive.ubuntu.com and not host security.ubuntu.com"
+```
 
 """
 
-import geni.portal as portal
-import geni.rspec.pg as rspec
-import geni.urn as URN
-import geni.rspec.igext as IG
-import geni.rspec.emulab.pnext as PN
-import geni.rspec.emulab as emulab
-
+BIN_PATH = "/local/repository/"
+ETC_PATH = "/local/repository/etc"
+UBUNTU_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+# COTS_UE_IMG = "urn:publicid:IDN+emulab.net+image+PowderTeam:cots-jammy-image"
+COMP_MANAGER_ID = "urn:publicid:IDN+emulab.net+authority+cm"
+DEFAULT_NR_CN_HASH = "v2.1.9"
+# DEFAULT_NR_RAN_HASH = "f3eb713084e4134ca265f1153b68a102714a319a" # 2025.wk15
 OAI_CN5G_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-oai-cn5g.sh")
 
+
 pc = portal.Context()
-request = pc.makeRequestRSpec()
 
-# Optional physical type for all nodes.
-pc.defineParameter("phystype",  "Optional hardware type",
-                   portal.ParameterType.STRING, "d430",
-                   longDescription="Specify hardware type (d430 or d820)")
+node_types = [
+    ("d430", "Emulab, d430"),
+    ("d710", "Emulab, d710"),
+]
 
 pc.defineParameter(
-    name="repo_url", 
-    description="GitHub repo for CN5G", 
+    name="cn_nodetype",
+    description="Type of compute node to use for CN node (if included)",
     typ=portal.ParameterType.STRING,
-    defaultValue="https://github.com/saimeghanakuchana/ReorderingOAI.git"
-    )
+    defaultValue=node_types[0],
+    legalValues=node_types
+)
 
-pc.defineParameter(
-    name="repo_branch", 
-    description="Branch or commit hash", 
-    typ=portal.ParameterType.STRING, 
-    defaultValue="main"
-    )
+# portal.context.defineStructParameter(
+#     "freq_ranges", "Frequency Ranges To Transmit In",
+#     defaultValue=[{"freq_min": 3550.0, "freq_max": 3600.0}],
+#     multiValue=True,
+#     min=0,
+#     multiValueTitle="Frequency ranges to be used for transmission.",
+#     members=[
+#         portal.Parameter(
+#             "freq_min",
+#             "Frequency Range Min",
+#             portal.ParameterType.BANDWIDTH,
+#             3550.0,
+#             longDescription="Values are rounded to the nearest kilohertz."
+#         ),
+#         portal.Parameter(
+#             "freq_max",
+#             "Frequency Range Max",
+#             portal.ParameterType.BANDWIDTH,
+#             3600.0,
+#             longDescription="Values are rounded to the nearest kilohertz."
+#         ),
+#     ]
+# )
 
-# Retrieve the values the user specifies during instantiation.
 params = pc.bindParameters()
 pc.verifyParameters()
+request = pc.makeRequestRSpec()
 
-node = request.RawPC("node")
-node.hardware_type = params.phystype
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+# role = "cn"
+
+# CN5G Host
+cn_node = request.RawPC("cn5g-host")
+cn_node.component_manager_id = COMP_MANAGER_ID
+cn_node.hardware_type = params.cn_nodetype
+cn_node.disk_image = UBUNTU_IMG
+
+# CN5G Interface + Subnet
+# cn_if = cn_node.addInterface("cn-if")
+# cn_if.addAddress(rspec.IPv4Address("192.168.1.1", "255.255.255.0"))
+# cn_link = request.Link("cn-link")
+# cn_link.setNoBandwidthShaping()
+# cn_link.addInterface(cn_if)
+
+# if params.oai_cn_commit_hash:
+#     oai_cn_hash = params.oai_cn_commit_hash
+# else:
+#     oai_cn_hash = DEFAULT_NR_CN_HASH
+
+# cmd = "{} '{}' {}".format(OAI_CN5G_DEPLOY_SCRIPT, oai_cn_hash, role)
+# cn_node.addService(rspec.Execute(shell="bash", command=cmd))
 
 # CN5G Startup Script
-deploy_cmd = "local/repository/deploy-oai-cn5g.sh {} {}".format(params.repo_url, params.repo_branch)
+#deploy_cmd = "/local/repository/bin/deploy-oai-cn5g.sh {} {}".format(params.repo_url, params.repo_branch)
+deploy_cmd = "/local/repository/deploy-oai-cn5g.sh /local/repository/oai-cn5g-fed"
 cn_node.addService(rspec.Execute(shell="bash", command=deploy_cmd))
 
+# for frange in params.freq_ranges:
+#     request.requestSpectrum(frange.freq_min, frange.freq_max, 0)
 
-
+# Tour
 tour = IG.Tour()
 tour.Description(IG.Tour.MARKDOWN, tourDescription)
 tour.Instructions(IG.Tour.MARKDOWN, tourInstructions)
 request.addTour(tour)
 
-portal.context.printRequestRSpec()
+pc.printRequestRSpec(request)
+
+
+
+
+
